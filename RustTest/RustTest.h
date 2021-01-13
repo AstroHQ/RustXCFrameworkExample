c77dcc9a1c08761c951701fdf881dfcb6b5bbd83
Jeremy Knope <jeremy@astro-hq.com>
2021-01-13 18:19:43 -0500

    Initial Commit

---
+++ RustTest/RustTest.h
@@ -0,0 +1,18 @@
+//
+//  RustTest.h
+//  RustTest
+//
+//  Created by Jeremy Knope on 1/13/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RustTest.
+FOUNDATION_EXPORT double RustTestVersionNumber;
+
+//! Project version string for RustTest.
+FOUNDATION_EXPORT const unsigned char RustTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RustTest/PublicHeader.h>
+
+
